3fa549088b2a3a47c53e85864b9c68af4e9d2700
Fabian <fab.beringer@gmail.com>
2020-05-23 19:52:59 +0200

    add dropout to mlp example

--- model/mlp.py
+++ model/mlp.py
@@ -1,10 +1,10 @@
-from torch.nn import Module, Linear, ReLU, Sequential
+from torch.nn import Module, Linear, ReLU, Sequential, Dropout
 
 
 class MLP(Module):
     """Simple Multi-Layer Perceptron also known as Feed Forward Neural Network."""
 
-    def __init__(self, in_dim, h_dim, out_dim):
+    def __init__(self, in_dim, h_dim, out_dim, dropout):
         """Builds the MLP
 
         :param in_dim: dimension of the input vectors.
@@ -12,8 +12,10 @@ class MLP(Module):
         :param out_dim: output dimension.
         """
         super().__init__()
-        self.classifier = Sequential(Linear(in_dim, h_dim),
+        self.classifier = Sequential(Dropout(dropout),
+                                     Linear(in_dim, h_dim),
                                      ReLU(),
+                                     Dropout(dropout),
                                      Linear(h_dim, out_dim))
 
     def forward(self, inputs):
