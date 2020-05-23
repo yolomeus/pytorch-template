853fad008cac8e88af9b32839b2f67c69729acf5
Fabian <fab.beringer@gmail.com>
2020-05-23 18:56:41 +0200

    support general metric definitions in config

--- lightning_wrapper.py
+++ lightning_wrapper.py
@@ -4,8 +4,6 @@ from omegaconf import DictConfig
 from pytorch_lightning import LightningModule
 from torch.utils.data import DataLoader
 
-from metrics import Accuracy
-
 
 class LightningModel(LightningModule):
     """General model wrapper for training pytorch models using the pytorch-lightning library. This class is responsible
@@ -14,8 +12,7 @@ class LightningModel(LightningModule):
 
     def __init__(self, hparams: DictConfig):
         """
-
-        :param hparams: contains model hyperparameters and all training settings.
+        :param hparams: contains model hyperparameters and training settings.
         """
         super().__init__()
         self.hparams = dict(hparams)
@@ -23,6 +20,8 @@ class LightningModel(LightningModule):
         self.model = instantiate(hparams.model)
         self.loss = instantiate(hparams.loss)
         self.optimizer = instantiate(hparams.optimizer, self.model.parameters())
+
+        self.metrics = [instantiate(metric) for metric in hparams.metrics]
 
         self.dataset_conf = hparams.dataset
         self.train_conf = hparams.training
@@ -38,25 +37,29 @@ class LightningModel(LightningModule):
         logs = {'loss': loss}
         return {'loss': loss, 'log': logs, 'y_pred': y_pred, 'y_true': y_true}
 
-    def training_epoch_end(self, outputs):
-        y_pred, y_true = self._unpack_outputs('y_pred', outputs), self._unpack_outputs('y_true', outputs)
-        y_pred = torch.softmax(y_pred, dim=-1).argmax(dim=-1)
-
-        acc = Accuracy()(y_pred.cpu(), y_true.cpu())
-        logs = {'acc': acc}
-        return {'acc': acc, 'log': logs}
-
     def validation_step(self, batch, batch_idx):
         x, y_true = batch
         y_pred = self.model(x)
         return {'y_pred': y_pred, 'y_true': y_true}
 
+    def training_epoch_end(self, outputs):
+        y_pred, y_true = self._unpack_outputs('y_pred', outputs), self._unpack_outputs('y_true', outputs)
+
+        logs = {self._classname(metric): metric(y_pred, y_true) for metric in self.metrics}
+        loss = self.loss(y_pred, y_true)
+        logs['loss'] = loss
+
+        return {'log': logs}
+
     def validation_epoch_end(self, outputs):
         y_pred, y_true = self._unpack_outputs('y_pred', outputs), self._unpack_outputs('y_true', outputs)
 
+        val_logs = {'val_' + self._classname(metric): metric(y_pred, y_true) for metric in self.metrics}
+
         val_loss = self.loss(y_pred, y_true)
-        logs = {'val_loss': val_loss}
-        return {'val_loss': val_loss, 'log': logs}
+        val_logs['val_loss'] = val_loss
+
+        return {'val_loss': val_loss, 'log': val_logs}
 
     def train_dataloader(self):
         train_conf = self.train_conf
@@ -83,7 +86,18 @@ class LightningModel(LightningModule):
         """Get the values of each output dict at key.
 
         :param key: key that gets the values from each output dict.
-        :param outputs: a list of output dicts
+        :param outputs: a list of output dicts.
         :return: the concatenation of all output dict values at key.
         """
         return torch.cat(list(map(lambda x: x[key], outputs)))
+
+    @staticmethod
+    def _classname(obj, lower=True):
+        """Get the classname of an object.
+
+        :param obj: any python object.
+        :param lower: return the name in lowercase.
+        :return: the classname as string.
+        """
+        name = obj.__class__.__name__
+        return name.lower() if lower else name
--- metrics.py
+++ metrics.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 
+import torch
 from sklearn.metrics import accuracy_score
+from torch import Tensor
 
 
 class Metric(ABC):
     @abstractmethod
-    def compute(self, y_pred, y_true):
+    def compute(self, y_pred: Tensor, y_true: Tensor):
         """Compute the metric given predictions and labels.
 
         :param y_pred: predicted scores
@@ -13,10 +15,15 @@ class Metric(ABC):
         :return: the computed metric value
         """
 
-    def __call__(self, y_pred, y_true):
+    def __call__(self, y_pred: Tensor, y_true: Tensor):
         return self.compute(y_pred, y_true)
 
 
 class Accuracy(Metric):
-    def compute(self, y_pred, y_true):
-        return accuracy_score(y_true, y_pred)
+    """
+    Compute accuracy using scikit-learn's `accuracy_score`.
+    """
+
+    def compute(self, y_pred: Tensor, y_true: Tensor):
+        y_pred = torch.softmax(y_pred, dim=-1).argmax(dim=-1)
+        return accuracy_score(y_true.cpu(), y_pred.cpu())
