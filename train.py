15306b892404aac03782ba46b79068fe23d9941d
Fabian <fab.beringer@gmail.com>
2020-05-23 23:16:41 +0200

    add random seed option

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import hydra
+import numpy as np
+import torch
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer
 
@@ -8,6 +10,10 @@ from lightning_wrapper import LightningModel
 @hydra.main(config_path='conf/config.yaml')
 def train(cfg: DictConfig):
     """Train a pytorch model specified by the config file"""
+
+    torch.manual_seed(cfg.random_seed)
+    np.random.seed(cfg.random_seed)
+
     model = LightningModel(cfg)
     trainer = Trainer(max_epochs=cfg.training.epochs, gpus=cfg.gpus)
     trainer.fit(model)
