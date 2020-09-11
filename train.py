24f7df8147569c6653a4a424bf781e8c9391bdec
Fabian <fab.beringer@gmail.com>
2020-09-11 19:18:15 +0200

    add early stopping callback

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import hydra
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 from lightning_wrapper import LightningModel
 
@@ -16,14 +16,21 @@ def train(cfg: DictConfig):
     model = LightningModel(cfg)
 
     train_cfg = cfg.training
-    checkpoint_callback = ModelCheckpoint(save_top_k=train_cfg.save_ckpts,
-                                          monitor=train_cfg.monitor,
-                                          mode=train_cfg.mode)
+    model_checkpoint = ModelCheckpoint(save_top_k=train_cfg.save_ckpts,
+                                       monitor=train_cfg.monitor,
+                                       mode=train_cfg.mode,
+                                       verbose=True)
+
+    early_stopping = EarlyStopping(monitor=train_cfg.monitor,
+                                   patience=train_cfg.patience,
+                                   mode=train_cfg.mode,
+                                   verbose=True)
 
     trainer = Trainer(max_epochs=train_cfg.epochs,
                       gpus=cfg.gpus,
                       deterministic=True,
-                      checkpoint_callback=checkpoint_callback)
+                      checkpoint_callback=model_checkpoint,
+                      early_stop_callback=early_stopping)
 
     datamodule = instantiate(cfg.datamodule,
                              train_conf=cfg.training,
