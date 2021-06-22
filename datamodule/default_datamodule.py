e326b82f222ae2ada9a378952da5713636b2f0f8
Fabian <fab.beringer@gmail.com>
2021-06-22 12:54:11 +0200

    add overrideable method for defining custom collate functions

--- datamodule/default_datamodule.py
+++ datamodule/default_datamodule.py
@@ -47,22 +47,41 @@ class AbstractDefaultDataModule(LightningDataModule):
                               self._train_conf.batch_size,
                               shuffle=True,
                               num_workers=self._num_workers,
-                              pin_memory=self._pin_memory)
+                              pin_memory=self._pin_memory,
+                              collate_fn=self.build_collate_fn(DatasetSplit.TRAIN))
         return train_dl
 
     def val_dataloader(self):
         val_dl = DataLoader(self.val_ds,
                             self._test_conf.batch_size,
                             num_workers=self._num_workers,
-                            pin_memory=self._pin_memory)
+                            pin_memory=self._pin_memory,
+                            collate_fn=self.build_collate_fn(DatasetSplit.VALIDATION))
         return val_dl
 
     def test_dataloader(self):
         test_dl = DataLoader(self.test_ds,
                              self._test_conf.batch_size,
                              num_workers=self._num_workers,
-                             pin_memory=self._pin_memory)
+                             pin_memory=self._pin_memory,
+                             collate_fn=self.build_collate_fn(DatasetSplit.TEST))
         return test_dl
+
+    # noinspection PyMethodMayBeStatic
+    def build_collate_fn(self, split: DatasetSplit = None):
+        """Override to define a custom collate function. Build a function for collating multiple data instances into
+        a batch. Defaults to returning `None` since it's the default for DataLoader's collate_fn argument.
+
+        While different collate functions might be needed depending on the dataset split, in most cases the same
+        function can be returned for all data splits.
+
+        :param split: The split that the collate function is used on to build batches. Can be ignored when train and
+        test data share the same structure.
+        :return: a single argument function that takes a list of tuples/instances and returns a batch as tensor or a
+        tuple of multiple batch tensors.
+        """
+
+        return None
 
 
 class ClassificationDataModule(AbstractDefaultDataModule):
